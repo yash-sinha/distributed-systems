45f02acdac5a19c3b9a624b62db59e5d366fd47b
Yash Sinha <yash@Yashs-MacBook-Pro.local>
2017-09-14 15:02:26 -0400

    init

---
+++ project1/lib/project1.ex
@@ -0,0 +1,18 @@
+defmodule PROJECT1 do
+  @moduledoc """
+  Documentation for PROJECT1.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PROJECT1.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
